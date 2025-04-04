fd4732f9096e3497a13c9c1b2419455c149bfcf5
Gennady G <me@andaril.net>
2025-04-04 23:55:42 +0300

    набросал поля

--- resources/js/components/registration.vue
+++ resources/js/components/registration.vue
@@ -3,58 +3,79 @@
     <form>
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Personal Information</h2>
-                <p class="mt-1 text-sm/6 text-gray-600">Use a permanent address where you can receive mail.</p>
+                <h2 class="text-base/7 font-semibold text-gray-900">Персональные данные</h2>
+                <p class="mt-1 text-sm/6 text-gray-600">Нам необходимо понять кто вы и что умеете.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-3">
-                        <label for="first-name" class="block text-sm/6 font-medium text-gray-900">First name</label>
+                    <div class="sm:col-span-2">
+                        <label for="surname" class="block text-sm/6 font-medium text-gray-900">Фамилия</label>
                         <div class="mt-2">
-                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                        </div>
-                    </div>
-
-                    <div class="sm:col-span-3">
-                        <label for="last-name" class="block text-sm/6 font-medium text-gray-900">Last name</label>
-                        <div class="mt-2">
-                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                        </div>
-                    </div>
-
-                    <div class="sm:col-span-4">
-                        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                        </div>
-                    </div>
-
-                    <div class="col-span-full">
-                        <label for="street-address" class="block text-sm/6 font-medium text-gray-900">Street address</label>
-                        <div class="mt-2">
-                            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                        </div>
-                    </div>
-
-                    <div class="sm:col-span-2 sm:col-start-1">
-                        <label for="city" class="block text-sm/6 font-medium text-gray-900">City</label>
-                        <div class="mt-2">
-                            <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="region" class="block text-sm/6 font-medium text-gray-900">State / Province</label>
+                        <label for="first_name" class="block text-sm/6 font-medium text-gray-900">Имя</label>
+                        <div class="mt-2">
+                            <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-2">
+                        <label for="patronymic" class="block text-sm/6 font-medium text-gray-900">Отчество</label>
+                        <div class="mt-2">
+                            <input type="text" name="patronymic" id="patronymic" autocomplete="additional-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-2">
+                        <fieldset>
+                            <legend class="text-sm/6 font-semibold text-gray-900">Пол персонажа</legend>
+                            <div class="mt-2 space-y-0">
+                                <div class="flex items-center gap-x-3">
+                                    <input id="female" name="gender" type="radio" class="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden">
+                                    <label for="female" class="block text-sm/6 font-medium text-gray-900">Женский</label>
+                                </div>
+                                <div class="flex items-center gap-x-3">
+                                    <input id="male" name="gender" type="radio" class="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden">
+                                    <label for="male" class="block text-sm/6 font-medium text-gray-900">Мужской</label>
+                                </div>
+                                <div class="flex items-center gap-x-3">
+                                    <input id="secret-prison" name="gender" type="radio" class="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden">
+                                    <label for="secret-prison" class="block text-sm/6 font-medium text-gray-900">Не определился</label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="birth_date" class="block text-sm/6 font-medium text-gray-900">Дата рождения</label>
+                        <div class="mt-2">
+                            <input type="date" name="birth_date" id="birth_date" autocomplete="birthday" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="phone" class="block text-sm/6 font-medium text-gray-900">Номер телефона</label>
+                        <div class="mt-2">
+                            <input id="phone" name="phone" type="phone" autocomplete="phone" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="iphone 11 pro max"/>
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3 sm:col-start-1">
+                        <label for="region" class="block text-sm/6 font-medium text-gray-900">Регион</label>
                         <div class="mt-2">
                             <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
 
-                    <div class="sm:col-span-2">
-                        <label for="postal-code" class="block text-sm/6 font-medium text-gray-900">ZIP / Postal code</label>
+                    <div class="sm:col-span-3">
+                        <label for="city" class="block text-sm/6 font-medium text-gray-900">Город</label>
                         <div class="mt-2">
-                            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
+
                 </div>
             </div>
 
@@ -62,9 +83,9 @@
                 <h2 class="text-base/7 font-semibold text-gray-900">Обработка персональных данных</h2>
                 <p class="mt-1 text-sm/6 text-gray-600">Мы обработаем все ваши данные исходя из нашей политики персональных данных</p>
 
-                <div class="mt-0 space-y-10">
+                <div class="mt-2 space-y-10">
                     <fieldset>
-                        <div class="mt-1 space-y-6">
+                        <div class="mt-2 space-y-6">
                             <div class="flex gap-3">
                                 <div class="flex h-6 shrink-0 items-center">
                                     <div class="group grid size-4 grid-cols-1">
