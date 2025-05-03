d3803f3bc5aa1cf553ae5a2ccb2a311ff6651476
Alexey Malev <diewindowsdie@gmail.com>
2025-05-03 23:45:20 +0300

    Исправлено неправильное отображение направления сортировки

--- resources/js/components/SortOrderIndicator.vue
+++ resources/js/components/SortOrderIndicator.vue
@@ -8,7 +8,7 @@
                   d="M18.425 10.271C19.499 8.967 18.57 7 16.88 7H7.12c-1.69 0-2.618 1.967-1.544 3.271l4.881 5.927a2 2 0 0 0 3.088 0l4.88-5.927Z"
                   clip-rule="evenodd"/>
         </svg>
-        <svg v-if="isAsc()"
+        <svg v-if="isDesc()"
              class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
              xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
              viewBox="0 0 24 24">
@@ -16,7 +16,7 @@
                   d="M18.425 10.271C19.499 8.967 18.57 7 16.88 7H7.12c-1.69 0-2.618 1.967-1.544 3.271l4.881 5.927a2 2 0 0 0 3.088 0l4.88-5.927Z"
                   clip-rule="evenodd"/>
         </svg>
-        <svg v-if="isDesc()"
+        <svg v-if="isAsc()"
              class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
              xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
              viewBox="0 0 24 24">
